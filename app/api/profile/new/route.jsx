41caa215e9667c8a485c6739e194992b99a573c5
iamclement1 <iamnnamdiclement@gmail.com>
2023-05-09 15:58:35 +0100

    feats: posting or creating new account'

---
+++ app/api/profile/new/route.jsx
@@ -0,0 +1,27 @@
+import { connectToDB } from "@/utils/database";
+import Profile from "@/models/profile";
+export const POST =  async(req, res) => {
+   const { userId, prompt, tag } = await req.json();
+
+   try {
+      await connectToDB(); 
+      const newProfile = new Profile({
+         creator: userId,
+         prompt, 
+         tag
+      });
+      await newProfile.save();
+
+      return new Response(JSON.stringify(newProfile), {
+         status: 201,
+      })
+      // res.status(201).json({
+      //    message: "Profile created",
+      //    profile: newProfile
+      // });
+   } catch (error) {
+      return new Response("Failed to create a new prompt", {
+         status: 500
+      })
+   }
+}
