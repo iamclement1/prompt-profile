bb667c16c12cb9021a8f7399c5d94c5d6c89c2af
iamclement1 <iamnnamdiclement@gmail.com>
2023-05-09 09:18:14 +0100

    update: wrap component in provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,26 @@
-import { Nav } from '@/components';
+import { Nav, Provider } from '@/components';
 import '@/styles/global.css';
 
 export const metadata = {
    title: "Profile Prompt",
    description: "Share profile for better discovery"
 }
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
    return (
       <html>
          <body>
-            <div className='main'>
-               <div className='gradient' />
-            </div>
-            <main className='app'>
-               <Nav/>
-               {children}
-            </main>
+            <Provider>
+               <div className='main'>
+                  <div className='gradient' />
+               </div>
+               <main className='app'>
+                  <Nav />
+                  {children}
+               </main>
+            </Provider>
          </body>
       </html>
-      )
+   )
 }
 
 export default RootLayout;
