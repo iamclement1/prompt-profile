6e4f13f86010a6eaff1940f05c9bb3f81b7736e9
iamclement1 <iamnnamdiclement@gmail.com>
2023-05-09 15:58:51 +0100

    feats: update and fetch data

--- components/Feeds.jsx
+++ components/Feeds.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+'use client'
+import { useState, useEffect } from "react"
+import { ProfileCard } from "."
 
-const Feeds = () => {
+
+const ProfileCardList = ({data, handleTagClick}) => {
   return (
-    <div>Feeds</div>
+    <div className="mt-16 prompt_layout">
+      {data.map((profile) => (
+        <ProfileCard 
+        key={profile._id}
+        profile={profile}
+        handleTagClick={handleTagClick} />
+      ))}
+    </div>
+  )
+}
+const Feeds = () => {
+  const [searchText, setSearchText] = useState("");
+  const [profile, setProfile] = useState([]);
+  const handleSearchChange = (event) => {
+
+  }
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const response = await fetch('/api/profile');
+      const data = await response.json();
+
+      setProfile(data);
+    }
+
+    fetchProfile();
+  }, [])
+  return (
+    <section className="feed">
+      <form action=""
+      className="w-full relative flex-center ">
+        <input type="text" 
+        placeholder="Search for profile"
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        className="search_input peer" />
+      </form>
+
+      <ProfileCardList
+      data={profile}
+      handleTagClick={() => {}} />
+    </section>
   )
 }
 
