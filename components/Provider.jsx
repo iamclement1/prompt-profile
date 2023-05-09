aaa0cf4eb8891fc7dddd99f166f930639620fab3
iamclement1 <iamnnamdiclement@gmail.com>
2023-05-09 09:19:30 +0100

    update: user provider

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,7 +1,10 @@
-
-const Provider = () => {
+"use client";
+import { SessionProvider } from "next-auth/react"
+const Provider = ({ children, session}) => {
    return (
-      <div>Provider</div>
+      <SessionProvider session={session}>
+         {children}
+      </SessionProvider>
    )
 }
 
