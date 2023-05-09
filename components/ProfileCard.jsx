4f2b0ebae36550ec6273b2b4431c7471ea1a06d9
iamclement1 <iamnnamdiclement@gmail.com>
2023-05-09 15:59:50 +0100

    feats: display fetched profile on card

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -1,7 +1,21 @@
-
-const ProfileCard = () => {
+'use client';
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+const ProfileCard = ({ profile, handleTagClick, handleEdit, handleDelete }) => {
    return (
-      <div>ProfileCard</div>
+      <div className="prompt_card">
+         <div className="flex justify-between item-start gap-5">
+            <div>
+               {/* <Image src={profile.creator.image} 
+               alt="user-image"
+               width={40}
+               height={40}
+               className="rounded-full object-contain" /> */}
+            </div>
+         </div>
+      </div>
    )
 }
 
