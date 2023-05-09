5d020e24c77050f39ad195fa61355c6d63b7b376
iamclement1 <iamnnamdiclement@gmail.com>
2023-05-09 09:18:57 +0100

    update: layer utility added

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
         'primary-orange': '#FF5722',
       },
       '@layer utilities': {
-        '.shadow-[inset_10px_-50px_94px_0_rgb(199,': {
+        '.shadow-[inset 10px, -50px, 94px, 0, rgb(199,': {
           // Define your shadow styles here
           boxShadow: 'inset 10px -50px 94px 0 rgba(199, ...)',
         },
